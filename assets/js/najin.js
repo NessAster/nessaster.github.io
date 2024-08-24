0b3387880f078ed424cc0cb8bd2c5421622c1976
NessAster <enderdemon48@gmail.com>
2024-08-25 00:21:18 +0200

    avancement Najin

---
+++ assets/js/najin.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let profileImage = document.querySelector("#profileImage");
+    let memeImage    = document.querySelector("#memeImage");
+
+    profileImage.addEventListener('click', (event) => {
+        profileImage.classList.add("d-none");
+        memeImage.classList.remove("d-none");
+    });
+    memeImage.addEventListener('click', (event) => {
+        memeImage.classList.add("d-none");
+        profileImage.classList.remove("d-none");
+    });
+
+    let audio = document.getElementById("ost");
+    audio.volume = 0.2;
+})
