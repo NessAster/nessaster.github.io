90a8b7ec417fda4810e58f64f31eb1f0db15cd9d
NessGaster <enderdemon48@gmail.com>
2024-09-19 11:28:45 +0200

    commencement Devyat Garion

--- assets/js/audio-set.js
+++ assets/js/audio-set.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let audio = document.getElementById("ost");
-    audio.volume = 0.2;
-    audio.play();
+    if (audio) {
+        audio.volume = 0.2;
+        audio.play();
+    }
 });
