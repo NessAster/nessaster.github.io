de4a3aa0c17fee8394106f07d64f58feb30b8db1
NessGaster <enderdemon48@gmail.com>
2024-10-27 18:29:21 +0100

    debut de creation Esther Hokma

--- assets/js/audio-set.js
+++ assets/js/audio-set.js
@@ -2,6 +2,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let audio = document.getElementById("ost");
     if (audio) {
         audio.volume = 0.2;
-        audio.play();
     }
 });
